b5b37e20d32b97dab40af853963cfe81fb4b236a
mattia.rollo <ste.tia@hotmail.it>
2022-12-20 18:27:09 +0100

    style show page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container">
+<section class="container ">
     <h1>{{$comic->title}}</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+                    <div class="card-body">
+                      <h5 class="card-title">{{$comic->title}}</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
