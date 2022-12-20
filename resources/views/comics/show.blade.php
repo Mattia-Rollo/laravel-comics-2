d193ec7fa4830035876464675d6f5db4d405aa7b
mattia.rollo <ste.tia@hotmail.it>
2022-12-20 16:28:56 +0100

    add controller for Route resource 'comics'

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="container">
+    <h1>{{$product->title}}</h1>
+</section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div id="logo" class="">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+    </div>
+</header>
